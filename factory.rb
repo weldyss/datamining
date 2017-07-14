7e129d610ca4982d42e9618ceedf1929c0734a8f
Weldys Santos <weldyss@gmail.com>
2017-07-13 22:34:58 -0300

    fixing factory

--- factory.rb
+++ factory.rb
@@ -1,26 +1,43 @@
- require 'factory_girl'
+require 'factory_girl'
+require 'csv'
+require './helpers'
 
- class Person
-   attr_accessor :id, :debt_type, :value, 
-     :negotiating_value, :parcels, :civil_status, :sallary
- end
+class Person
+  attr_accessor :id, :debt_type, :debt_value, 
+    :negotiating_value, :parcels, :civil_status, :sallary
+end
 
- FactoryGirl.define do
-   factory :person do
-     document "001"
-     debt_type ["credit card", "financing", "retail", "bank"].sample
-     value (1000..92300).to_a.sample
-     negotiating_value value
-     parcels (1..25).to_a.sample
-     civil_status ["single", "married", "divorced", "other"].sample
-     sallary (900..5000).to_a.sample
-   end
- end
+FactoryGirl.define do
+  sequence :auto_id do |n| 
+    "#{n}"
+  end
+  factory :person do
+    id { FactoryGirl.generate(:auto_id) }
+    debt_type { ["credit card", "financing", "retail", "bank"].sample }
+    debt_value { (1000..92300).to_a.sample }
+    negotiating_value (:debt_value) 
+    parcels { (1..25).to_a.sample }
+    civil_status { ["single", "married", "divorced", "other"].sample }
+    sallary { (900..5000).to_a.sample }
+  end
+end
 
- person = FactoryGirl.build(:person)
- csv_string = CSV.generate do |csv| 
-   csv << ["nome", "tipo_da_divida", "valor", "valor_negociado", "parcelas", "estado_civil", "salario"]
- end
+def run_forrest 
+  print_memory_usage do
+    print_time_spent do
+      headers = ['id', 'debt_type', 'debt_value', 'negotiating_value', 'parcels', 'civil_status', 'sallary']
+      all_person = []
+      127_000.times do 
+        all_person << FactoryGirl.build(:person)
+      end
 
- puts csv_string
+      CSV.open('all_data.csv', 'w', write_headers: true, headers: headers) do |csv|
+        all_person.each do |i|
+          csv << [i.id, i.debt_type, i.debt_value, i.negotiating_value, i.parcels, i.civil_status, i.sallary]
+        end
+      end
+    end
+  end
+end
+
 
