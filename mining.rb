405e23e2bd2755385112e2bf1b4a3cdd7f27fbf3
Weldys Santos <weldyss@gmail.com>
2017-07-14 00:43:05 -0300

    starting mining and classifying

--- mining.rb
+++ mining.rb
@@ -1,3 +1,14 @@
 require 'ruby-band'
 
-dataset = Core::Parser.parse_CSV(dataset.csv)
+#Client.new(:id, :debt_type, :debt_value, :negotiating_value, :parcels, :civil_status, :sallary)
+
+dataset = Core::Parser.parse_CSV('all_data.csv')
+
+dataset = Core::Type::Instances::Base.new do
+  nominal :debt_type, ["credit card", "financing", "retail", "bank"]
+  numeric :debt_value
+  numeric :negotiating_value
+  numeric :parcels
+  nominal :civil_status, ["single", "married", "divorced", "other"]
+  numeric :sallary
+end
